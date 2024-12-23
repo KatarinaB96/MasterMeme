279b0aba740ce9c979d2f9e692508cac9fad1cad
Barış SEMERCİ <barissemerci3@gmail.com>
2024-12-23 19:23:23 +0300

    update change text dialog ui

--- app/src/main/java/com/campus/mastermeme/edit/presentation/components/ChangeTextDialog.kt
+++ app/src/main/java/com/campus/mastermeme/edit/presentation/components/ChangeTextDialog.kt
@@ -1,15 +1,20 @@
 package com.campus.mastermeme.edit.presentation.components
 
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.campus.mastermeme.R
 
 @Composable
@@ -24,12 +29,20 @@ fun ChangeTextDialog(
         onDismissRequest = {
             onDismissDialog()
         },
-        title = { Text("Metin Düzenle") },
+        containerColor = MaterialTheme.colorScheme.surface,
+        title = { Text(stringResource(R.string.text), fontSize = 24.sp) },
         text = {
             TextField(
                 value = editableText,
                 onValueChange = { editableText = it },
-                singleLine = true
+                singleLine = true,
+                colors = TextFieldDefaults.colors().copy(
+                    focusedIndicatorColor = MaterialTheme.colorScheme.secondary,
+                    unfocusedIndicatorColor = MaterialTheme.colorScheme.secondary,
+                    cursorColor = MaterialTheme.colorScheme.secondary,
+                    focusedContainerColor = MaterialTheme.colorScheme.surface,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                )
             )
         },
         confirmButton = {
@@ -38,14 +51,15 @@ fun ChangeTextDialog(
                     selectedTextIndex,
                     editableText
                 )
-
+                onDismissDialog()
             }) {
-                Text("Kaydet")
+                Text(stringResource(R.string.ok), color = MaterialTheme.colorScheme.secondary)
             }
         },
+        shape = RoundedCornerShape(8.dp),
         dismissButton = {
             TextButton(onClick = { onDismissDialog() }) {
-                Text(stringResource(R.string.cancel))
+                Text(stringResource(R.string.cancel), color = MaterialTheme.colorScheme.secondary)
             }
         }
     )
